29f17e469ab9b313554ba887d472ed8e091440cd
Kaiba <yt.kaibaoke@gmail.com>
2025-10-12 18:15:09 +0900

    Add data field to UDP response in udp_receiver.py for image URL inclusion

--- processor/udp_receiver.py
+++ processor/udp_receiver.py
@@ -86,6 +86,7 @@ def process_received_data(data: Dict[Any, Any]) -> None:
         "type": "response",
         "timestamp": time.strftime("%Y-%m-%dT%H:%M:%SZ", time.gmtime()),
         "original_data": data,
+        "data": "https://firebasestorage.googleapis.com/v0/b/techbias.firebasestorage.app/o/CFI_Ad-03.jpg?alt=media&token=f1a60652-069a-4985-a357-1dd28e1376cf",
         "processed": True,
         "message": "データ処理が完了しました"
     }
